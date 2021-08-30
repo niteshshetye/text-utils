433c0c4b6e054a6e987ab2bb14b4cf001fa01b50
Nitesh Shetye <niteshshetye@Niteshs-Mac-mini.local>
2021-08-30 12:21:31 +0530

    some responsive changes made added endable dark mode in mobile

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,16 +4,10 @@ import {Link} from 'react-router-dom'
 export default function NavBar(props) {
     
     return (
-        <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+        <nav className={`navbar navbar-expand navbar-${props.mode} bg-${props.mode}`}>
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/">Text Utils</Link>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-                        </li>
-                        
-                    </ul>
                     <div className={props.mode === 'dark'? "form-check form-switch text-light": 'form-check form-switch'}>
                         <input className="form-check-input" type="checkbox" id="mode" onClick={props.enableDarkMode} />
                         <label className="form-check-label" htmlFor="mode">Enable Dark Mode</label>
